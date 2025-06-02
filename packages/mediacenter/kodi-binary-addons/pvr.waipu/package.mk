2e6d71f340844829009f5fbfb75790fee12c3131
Rudi Heitbaum <rudi@heitbaum.com>
2025-06-02 02:51:33 +0000

    pvr.waipu: update 21.11.0-Omega to 21.11.1-Omega

--- packages/mediacenter/kodi-binary-addons/pvr.waipu/package.mk
+++ packages/mediacenter/kodi-binary-addons/pvr.waipu/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2019-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="pvr.waipu"
-PKG_VERSION="21.11.0-Omega"
-PKG_SHA256="0cfc60382d647fac0c248059ff3012d48b7d6449ce094a841a3a29fd501ac4f9"
+PKG_VERSION="21.11.1-Omega"
+PKG_SHA256="14a549caa2502404df6bbdec973f85836ae0becf266fc736f68ace8b911dd856"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
